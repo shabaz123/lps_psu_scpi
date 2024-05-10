c61474588d08746321b4449dfe82e3cc997e86f1
shabaz123 <shabaz123@users.noreply.github.com>
2024-05-10 14:13:19 +0100

    Update psu_utils.h

--- source/psu/psu_utils.h
+++ source/psu/psu_utils.h
@@ -18,10 +18,6 @@
 
 /********* functions *********/
 void initPsuUtils(); // configures the PSU feature
-//uint32_t psuPinCount(); // returns the number of PSU pins (always 3)
-//void initPsuPins(); // sets up the pin mode to PSU
-//void setPsuVoltage(uint32_t index, float value); // sets the PSU channel (0-3) voltage to a float value in Volts
-//float getPsuVoltage(uint32_t index); // returns the PSU channel voltage level in Volts
 
 scpi_result_t SCPI_PsuVoltage(scpi_t * context);
 scpi_result_t SCPI_PsuVoltageQ(scpi_t * context);
